34c64ea946cd3003da25cf3805a89b98739b3153
araiarai55555 <araiarai55555@yahoo.co.jp>
2021-02-06 14:47:49 +0900

    0206

--- test01.py
+++
@@ -1,9 +0,0 @@
-result = []
-for num1 in range(1, 10):
-    tmp = []
-    for num2 in range(1, 10):
-        tmp.append(f'{num1 * num2:2}')
-    result.append(tmp)
-
-for row in result:
-    print(', '.join(row))
