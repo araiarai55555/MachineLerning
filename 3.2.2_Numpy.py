f9fdfd05fc0349a95488cdfeab26f4ca4633b114
araiarai55555 <araiarai55555@yahoo.co.jp>
2021-02-07 16:29:24 +0900

    0207

---
+++ 3.2.2_Numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+x = np.array([-1.0,1.0,2.0])
+print (x)
+y = x > 0
+print (y)
+y = y.astype(np.int)
+print (y)
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
