2036b09488067448c6cc5291c9935180813275f5
araiarai55555 <araiarai55555@yahoo.co.jp>
2021-02-09 20:07:57 +0900

    0233

---
+++ 3.5.1_SoftMax.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def softmax(a):
+    c = np.max(a)
+
+    exp_a = np.exp(a-c)
+ 
+    sum_exp_a = np.sum(exp_a)
+
+    y = exp_a / sum_exp_a
+
+
+    return y
+
+
+
+#a = np.array([0.3,2.9,4.0])
+a = np.array([1010,1000,990])
+print(softmax(a))
+print(np.sum(softmax(a)))
